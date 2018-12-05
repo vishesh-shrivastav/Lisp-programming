efc9f23dc7840f47e4910ee1fe13d134a3a68a3f
Vishesh <vishesh2k6@gmail.com>
2018-12-05 03:38:03 -0600

    Add files via upload

--- functionatom.lisp
+++ functionatom.lisp
@@ -1,6 +1,13 @@
-(defun  foo(l)
-	(cond
-		((null l) nil)
-		((atom l) (list l))
-		(t (append (foo (car l)) ; trying with append
-				 (foo (cdr l))))))
+;; Custom function for concatenating two lists
+(defun concat (li1 li2)
+  ; If first list is empty, return second list
+  (cond ((null li1) li2)
+  		; Else, store first element of list one and recursively "concat" rest of list 1 and list 2 
+    	(t (cons (car li1) (concat (cdr li1) li2))))) 
+
+;; Function for flattening a list - uses 'concat' defined above
+(defun functionatom (x)
+  (cond ((null x) x) ;; Base condition - if list is empty, return empty list
+        ((atom x) (list x)) ;; If list has only one element, return it
+        ;; Else if flattening pairs, return a list by concatenating the flattening of its car with the flattening of its cdr
+        (t (concat (functionatom (car x)) (functionatom (cdr x))))))
